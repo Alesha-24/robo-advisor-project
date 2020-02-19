e72201a80a890dfd19ca65b094b3bb14f5210cb6
Alesha-24 <59664172+Alesha-24@users.noreply.github.com>
2020-02-18 22:35:33 -0500

    update

---
+++ app/robo_advisor.py
@@ -0,0 +1,8 @@
+import requests
+import json 
+
+print("REQUESTING SOME SATA FROM THE INTERNET...")
+API_KEY = "HC0CX05F78ER757D"
+
+symbol = input("Please enter the ticker symbol for the stock data that you want: ")
+
