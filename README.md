dd02ca6755322b3a630acfb914866337f3e43c02
Alesha-24 <59664172+Alesha-24@users.noreply.github.com>
2020-02-20 16:53:04 -0500

    Update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # robo-advisor-project
+
+Creating a vritual environment gives you a place to put all the packages in code in - allows dinstinct set up for each project 
+The purpose of this program is simple: the system should accept one or more stock or cryptocurrency symbols as information inputs, then it should request real live historical trading data from the Internet, and finally it should provide a recommendation as to whether or not the client should purchase the given stocks or cryptocurrencies.
+
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,8 +1,46 @@
 import requests
 import json 
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 print("REQUESTING SOME SATA FROM THE INTERNET...")
-API_KEY = "HC0CX05F78ER757D"
+print("Hello and welcome to this Financial Planning Software")
 
 symbol = input("Please enter the ticker symbol for the stock data that you want: ")
+if symbol.isalpha() and len(symbol) <=4: 
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    print(("URL:", request_url))
+    response = requests.get(request_url)
+    #print(response.text)
+    if "Error Message" in response.text:
+        print("ERROR! That is an invalid ticker, please try again")
+        exit()
+    print(response.status_code)
+    risk = input("What is your risk tolerance, please enter LOW, MEDIUM or HIGH: ")
+else:
+    print("Error - expecting a properly formed stock symbol like MSFT. Please try again")
 
+parsed_response = json.loads(response.text)
+print(parsed_response)
+
+
+print("-------------------------")
+print("SELECTED SYMBOL: ",symbol)
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print("LATEST DAY: 2018-02-20")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
