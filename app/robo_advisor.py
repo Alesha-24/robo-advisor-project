b76744f1f8843bffc3562b65e20a301fdb395467
Alesha-24 <59664172+Alesha-24@users.noreply.github.com>
2020-02-21 17:38:54 -0500

    Update robo_advisor.py

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,6 +3,9 @@ import json
 import os 
 from dotenv import load_dotenv
 
+def to_usd(my_price):
+    return f"${my_price:,.2f}"
+
 load_dotenv()
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
@@ -12,21 +15,26 @@ print("Hello and welcome to this Financial Planning Software")
 
 symbol = input("Please enter the ticker symbol for the stock data that you want: ")
 if symbol.isalpha() and len(symbol) <=4: 
-    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey={API_KEY}"
     print(("URL:", request_url))
     response = requests.get(request_url)
+    print(response.text)
     #print(response.text)
     if "Error Message" in response.text:
         print("ERROR! That is an invalid ticker, please try again")
         exit()
-    print(response.status_code)
     risk = input("What is your risk tolerance, please enter LOW, MEDIUM or HIGH: ")
 else:
     print("Error - expecting a properly formed stock symbol like MSFT. Please try again")
 
 parsed_response = json.loads(response.text)
 print(parsed_response)
-
+print(parsed_response.keys())
+last_refreshed = (parsed_response["Meta Data"]["3. Last Refreshed"])
+tsd = (parsed_response["Time Series (Daily)"])
+date = list(tsd.keys())
+latest_day = date[0]
+latest_close = tsd[latest_day]["4. close"]
 
 print("-------------------------")
 print("SELECTED SYMBOL: ",symbol)
@@ -34,8 +42,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print("LATEST DAY: ", last_refreshed)
+print("LATEST CLOSE: ", to_usd(float(latest_close)))
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
