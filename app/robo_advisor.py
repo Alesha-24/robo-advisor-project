dd67013418d595929c8c96d10a70ca974b576244
Alesha-24 <59664172+Alesha-24@users.noreply.github.com>
2020-02-24 17:35:41 -0500

    Update

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,6 +3,8 @@ import json
 import os 
 from dotenv import load_dotenv
 import csv 
+import plotly
+import plotly.graph_objs as go
 
 load_dotenv()
 
@@ -17,7 +19,6 @@ today = datetime.now()
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
-print("REQUESTING SOME SATA FROM THE INTERNET...")
 print("Hello and welcome to this Financial Planning Software")
 
 
@@ -27,7 +28,7 @@ while True:
         print("-------------------------")
         print("THANK YOU FOR INVESTING")
         print("-------------------------")
-        
+        exit()
         
     elif symbol.isalpha() and len(symbol) <=4:
         request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=compact&apikey={API_KEY}"
@@ -35,13 +36,13 @@ while True:
         response = requests.get(request_url)
         print(response.text)
         if "Error Message" in response.text:
-            print("ERROR! That is an invalid ticker, please try again")
+            print("ERROR! That is an invalid ticker!")
             exit()
       #add something in such that risk tolerance changes the reccomendation 
         else: 
             parsed_response = json.loads(response.text)
-            print(parsed_response)
-            print(parsed_response.keys())
+            #print(parsed_response)
+            #print(parsed_response.keys())
             last_refreshed = (parsed_response["Meta Data"]["3. Last Refreshed"])
             tsd = (parsed_response["Time Series (Daily)"])
             dates = list(tsd.keys())
@@ -60,13 +61,33 @@ while True:
             recent_low = max(low_prices)
             
             risk = input("What is your risk tolerance, please enter LOW, MEDIUM or HIGH: ")
+
+            if risk == "LOW"
+                float(latest_close) >= 1.2 * float(recent_low): 
+                    reccomendation = "BUY"
+                else:
+                    reccomendation = "DON'T BUY"
+            elif risk == "MEDIUM"
+            elif risk == "HIGH"
             
-            if float(latest_close) >= 1.2 * float(recent_low): 
-                reccomendation = "BUY"
-            else:
-                reccomendation = "DON'T BUY"
             
-            #add something else in 
+            file_name = "prices_" + symbol 
+            print(file_name)
+            csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", file_name + ".csv" )
+
+            with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+                writer = csv.DictWriter(csv_file, fieldnames=["timestamp", "open", "high", "low", "close", "volume"])
+                writer.writeheader() # uses fieldnames set above
+                for date in dates: 
+                    daily_prices = tsd[date]
+                    writer.writerow({
+                        "timestamp": date, 
+                        "open": to_usd(float(daily_prices["1. open"])),
+                        "high": to_usd(float(daily_prices["2. high"])),
+                        "low": to_usd(float(daily_prices["3. low"])),
+                        "close": to_usd(float(daily_prices["4. close"])),
+                        "volume": daily_prices["5. volume"]
+                })
             print("-------------------------")
             print("SELECTED SYMBOL: ",symbol)
             print("-------------------------")
@@ -85,26 +106,35 @@ while True:
             #need to write data to CSV file 
             print("WRITING DATA TO") #csv_file_path)
             print("-------------------------")
+
+            for date in dates:
+                line_data = [
+                    {"date": date, "stock_price_usd": daily_prices["4. close"],
+                    }]
+            x_axis = []
+            y_axis = []
+            for x in line_data:
+                x_coord = x["date"]
+                y_coord = x["stock_price_usd"]
+                x_axis.append(x_coord)
+                y_axis.append(y_coord)
+                
+            plotly.offline.plot({
+                "data": [go.Scatter(x= x_axis, y=y_axis)],
+                "layout": go.Layout(title="Stock Prices")
+            }, auto_open=True)
+
+
+            print("----------------")
+            print("GENERATING LINE GRAPH...")
+            print(line_data) 
             print("PROCESSING NEXT REQUEST...")
 
     else: 
         print("Error - expecting a properly formed stock symbol like MSFT. Please try again")
 
-
 #check low and high price difference
-#to do = calcualte 52 week highs and lows s
-
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
-    writer.writeheader() # uses fieldnames set above
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
-
-#way ton incorporate multiple stock into analysis 
-
-
-#todo = print grapph of stock price over time
+#to do = calcualte 52 week highs and lows 
+#todo = add to the read me 
+#todo = print graph of stock price over time
+#REFINE RECCOMENDATION based on risk level 
