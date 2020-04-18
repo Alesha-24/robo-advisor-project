f05ac97af2d6d8d511828eaf2e754bc9aedc3921
Alesha-24 <59664172+Alesha-24@users.noreply.github.com>
2020-04-17 20:18:09 -0400

    Testing configuration

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -9,29 +9,30 @@ import statistics
 
 load_dotenv()
 
-from datetime import date
-from datetime import time 
 from datetime import datetime
 
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "DEMO")
 def get_response1(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=compact&apikey={API_KEY}"
     response = requests.get(request_url)
-    return response
+    parsed_response = json.loads(response.text)
+    return response, parsed_response
 
 def get_response2(symbol):
     request_52 = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={symbol}&apikey={API_KEY}"
     response_52 = requests.get(request_52)
-    return response_52
+    parsed_response_52 = json.loads(response_52.text)
+    return parsed_response_52
 
-def calculate_recent_high(tsd):
+def calculate_recent_high(tsd,dates):
     high_prices = []
-    for  date in dates:
+    for date in dates:
         high_price = tsd[date]["2. high"]
         high_prices.append(high_price)
     recent_high = max(high_prices)
     return recent_high
 
-def calculate_recent_low(tsd):
+def calculate_recent_low(tsd, dates):
     low_prices = []
     for  date in dates:
         low_price = tsd[date]["3. low"]
@@ -39,7 +40,7 @@ def calculate_recent_low(tsd):
     recent_low = min(low_prices)
     return recent_low
 
-def write_to_csv(csv_file_path):
+def write_to_csv(tsd, csv_file_path, dates):
     csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
     with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
         writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
@@ -55,7 +56,7 @@ def write_to_csv(csv_file_path):
                 "volume": daily_prices["5. volume"]})
     return True 
 
-def risk_calc(risk):
+def risk_calc(recent_low, latest_close, avg_52_week, risk):
     if risk == "LOW" and float(latest_close) >= 1.2 * float(recent_low) and float(latest_close) > avg_52_week :
         recc = "BUY"
     elif risk == "MEDIUM" and float(latest_close) >= 1.12 * float(recent_low) and float(latest_close) > avg_52_week :
@@ -91,8 +92,6 @@ if __name__ == "__main__":
 
     today = datetime.now()
 
-    API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS")
-
     print("-------------------------")
     print("HELLO AND WELCOME TO THIS STOCK ANALYSIS AND INVESTMENT RECCOMENDATION PROGRAM")
     print("-------------------------")
@@ -106,25 +105,24 @@ if __name__ == "__main__":
             exit()
             
         elif symbol.isalpha() and len(symbol) <=4:
-            response = get_response1(symbol)
+            response, _ = get_response1(symbol)
             if "Error Message" in response.text:
                 print("ERROR! That is an invalid ticker!")
                 exit()
             else: 
-                parsed_response = json.loads(response.text)
+                _ , parsed_response = get_response1(symbol)
                 last_refreshed = (parsed_response["Meta Data"]["3. Last Refreshed"])
                 tsd = (parsed_response["Time Series (Daily)"])
                 dates = list(tsd.keys())
                 latest_day = dates[0]
                 latest_close = tsd[latest_day]["4. close"]
-                
-                response_52 = get_response2(symbol)
-                parsed_response_52 = json.loads(response_52.text)
+
+                parsed_response_52 = get_response2(symbol)
                 tsw = (parsed_response_52["Weekly Adjusted Time Series"])
                 weeks = list(tsw.keys())
                 
-                recent_high = calculate_recent_high(tsd)
-                recent_low = calculate_recent_low(tsd)
+                recent_high = calculate_recent_high(tsd,dates)
+                recent_low = calculate_recent_low(tsd,dates)
                 
                 close_prices = []
                 highs_52 = []
@@ -143,12 +141,12 @@ if __name__ == "__main__":
                 avg_52_week = statistics.mean(close_prices)
                 
                 risk = input("What is your risk tolerance for this stock, please enter LOW, MEDIUM or HIGH: ")
-                recc = risk_calc(risk)
+                recc = risk_calc(recent_low, risk,latest_close,avg_52_week)
                 reccomendation_reason = recc_reason(risk, recc)
     
                 file_name = "prices_" + symbol 
                 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", file_name + ".csv" )
-                write_to_csv(csv_file_path)
+                write_to_csv(tsd, csv_file_path, dates)
                 
                 formatted_time = datetime.now().strftime("%m/%d/%Y, %r")
 
--- test/my_test.py
+++ test/my_test.py
@@ -1,7 +1,102 @@
 import pytest 
+import os 
+from app.robo_advisor import get_response1, get_response2, calculate_recent_high, calculate_recent_low, write_to_csv, risk_calc, recc_reason, to_usd 
 
-from app.robo_advisor import to_usd 
+CI_ENV = os.environ.get("CI") == "true" # expect default environment variable setting of "CI=true" on Travis CI, see: https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+@pytest.mark.skipif(CI_ENV==True, reason="to avoid configuring credentials on, and issuing requests from, the CI server")
 
+def test_get_response1():
+    symbol = "TSLA"
+    _, parsed_response = get_response1(symbol)
+    assert isinstance(parsed_response, dict)
+    assert "Meta Data" in parsed_response.keys()
+    assert "Time Series (Daily)" in parsed_response.keys()
+    assert parsed_response["Meta Data"]["2. Symbol"] == symbol
+
+def test_get_response2():
+    symbol = "TSLA"
+    parsed_response_2 = get_response2(symbol)
+    assert isinstance(parsed_response_2, dict)
+    assert "Meta Data" in parsed_response_2.keys()
+    #assert "Weekly Adjusted Prices and Volumes" in parsed_response_2
+    assert parsed_response_2["Meta Data"]["2. Symbol"] == symbol
+
+def test_calculate_recent_high():
+    tsd = {'2020-04-17': {'1. open': '179.5000', '2. high': '180.0000', '3. low': '175.8700', '4. close': '178.6000', '5. volume': '52273542'},
+           '2020-04-16': {'1. open': '174.3000', '2. high': '177.2800', '3. low': '172.9000', '4. close': '177.0400', '5. volume': '50479610'}, 
+           '2020-04-15': {'1. open': '171.2000', '2. high': '173.5700', '3. low': '169.2400', '4. close': '171.8800', '5. volume': '40940833'}, 
+           '2020-04-14': {'1. open': '169.0000', '2. high': '173.7500', '3. low': '168.0000', '4. close': '173.7000', '5. volume': '52874338'}, 
+           '2020-04-13': {'1. open': '164.3500', '2. high': '165.5700', '3. low': '162.3000', '4. close': '165.5100', '5. volume': '41905264'}, 
+           '2020-04-09': {'1. open': '166.3600', '2. high': '167.3700', '3. low': '163.3300', '4. close': '165.1400', '5. volume': '51431775'}
+           }
+    dates = list(tsd.keys())
+    assert calculate_recent_high(tsd,dates) == "180.0000"
+
+def test_calculate_recent_low():
+    tsd = {'2020-04-17': {'1. open': '179.5000', '2. high': '180.0000', '3. low': '175.8700', '4. close': '178.6000', '5. volume': '52273542'},
+           '2020-04-16': {'1. open': '174.3000', '2. high': '177.2800', '3. low': '172.9000', '4. close': '177.0400', '5. volume': '50479610'}, 
+           '2020-04-15': {'1. open': '171.2000', '2. high': '173.5700', '3. low': '169.2400', '4. close': '171.8800', '5. volume': '40940833'}, 
+           '2020-04-14': {'1. open': '169.0000', '2. high': '173.7500', '3. low': '168.0000', '4. close': '173.7000', '5. volume': '52874338'}, 
+           '2020-04-13': {'1. open': '164.3500', '2. high': '165.5700', '3. low': '162.3000', '4. close': '165.5100', '5. volume': '41905264'}, 
+           '2020-04-09': {'1. open': '166.3600', '2. high': '167.3700', '3. low': '163.3300', '4. close': '165.1400', '5. volume': '51431775'}
+           }
+    dates = list(tsd.keys())
+    assert calculate_recent_low(tsd,dates) == "162.3000"
+
+def test_write_to_csv():
+    csv_filepath = os.path.join(os.path.dirname(__file__), "example_reports", "temp_prices.csv")
+    tsd = {'2020-04-17': {'1. open': '179.5000', '2. high': '180.0000', '3. low': '175.8700', '4. close': '178.6000', '5. volume': '52273542'},
+           '2020-04-16': {'1. open': '174.3000', '2. high': '177.2800', '3. low': '172.9000', '4. close': '177.0400', '5. volume': '50479610'}, 
+           '2020-04-15': {'1. open': '171.2000', '2. high': '173.5700', '3. low': '169.2400', '4. close': '171.8800', '5. volume': '40940833'}, 
+           '2020-04-14': {'1. open': '169.0000', '2. high': '173.7500', '3. low': '168.0000', '4. close': '173.7000', '5. volume': '52874338'}, 
+           '2020-04-13': {'1. open': '164.3500', '2. high': '165.5700', '3. low': '162.3000', '4. close': '165.5100', '5. volume': '41905264'}, 
+           '2020-04-09': {'1. open': '166.3600', '2. high': '167.3700', '3. low': '163.3300', '4. close': '165.1400', '5. volume': '51431775'}
+           }
+    dates = ['2020-04-17', '2020-04-16', '2020-04-15', '2020-04-14', '2020-04-13', '2020-04-09', '2020-04-08', '2020-04-07']
+    if os.path.isfile(csv_filepath):
+        os.remove(csv_filepath)
+    assert os.path.isfile(csv_filepath) == False # just making sure the test was setup properly
+    # INVOCATION
+    result = write_to_csv(tsd, csv_filepath, dates)
+    # EXPECTATIONS
+    assert result == True
+    assert os.path.isfile(csv_filepath) == True
+
+def test_risk_calc():
+    #ensuring that the function is calculating risk level appropriately
+    latest_close = 22
+    recent_low = 18
+    avg_52_week = 20
+    risk = "LOW"
+    assert risk_calc(recent_low, latest_close, avg_52_week, risk) == "BUY"
+    risk = "MEDIUM"
+    assert risk_calc(recent_low, latest_close, avg_52_week, risk) == "BUY"
+    risk = "HIGH"
+    assert risk_calc(recent_low, latest_close, avg_52_week, risk) == "BUY"
+
+    latest_close = 19
+    recent_low = 18
+    avg_52_week = 20
+    risk = "LOW"
+    assert risk_calc(recent_low, latest_close, avg_52_week, risk) == "DON'T BUY"
+    risk = "MEDIUM"
+    assert risk_calc(recent_low, latest_close, avg_52_week, risk) == "DON'T BUY"
+    risk = "HIGH"
+    assert risk_calc(recent_low, latest_close, avg_52_week, risk) == "DON'T BUY"
+
+def test_recc_reason():
+    risk = "LOW" 
+    recc = "BUY"
+    assert recc_reason(risk, recc) == "The stock's latest close price was 1.20 times higher than the lowest price in the past 100 days. Also, the latest close price was greater than the average close price over the past 52 weeks. Therefore, this stock seems profitable and our reccommendation is to buy it."
+    risk = "MEDIUM" 
+    recc = "BUY"
+    assert recc_reason(risk, recc) == "The stock's latest close price was 1.12 times higher than the lowest price in the past 100 days. Also, the latest close price was greater than the average close price over the past 52 weeks. Therefore, this stock seems profitable and our reccomemndation is to buy it." 
+    risk = "HIGH" 
+    recc = "BUY"
+    assert recc_reason(risk, recc) == "The stock's latest close price was 1.07 times higher than the lowest price in the past 100 days. Also, the latest close price was greater than the average close price over the past 52 weeks. Therefore, this stock seems profitable and our reccommendation is to buy it."
+    recc = "DON'T BUY"
+    assert recc_reason(risk, recc) == "This stock has not shown signfnicant growth potential and the market seems to be quite volatile. Therefore, we advise not to buy this stock at this time."
+ 
 def test_to_usd(): 
     # it should apply USD formatting
     assert to_usd(2.40) == "$2.40"
